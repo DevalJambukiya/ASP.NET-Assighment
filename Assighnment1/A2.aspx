a49d5d86a811ed3cb192bd8f700d907695145afb
DevalJambukiya <jambukiyadeval@gmail.com>
2024-12-26 14:01:22 +0530

    Temperature Conversion A1 q2

--- Assighnment1/A2.aspx
+++ Assighnment1/A2.aspx
@@ -1,14 +1,34 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="A2.aspx.cs" Inherits="Assighment.Assighnment1.A2" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Q2.aspx.cs" Inherits="Tutorial.Assignment_1.Q2" %>
 
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Temperature Conversion</title>
+  
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <h2>Temperature Conversion</h2>
+            
+            <!-- Input for Temperature -->
+            <asp:Label ID="lblTemperature" runat="server" Text="Enter Temperature:" />
+            <asp:TextBox ID="txtTemperature" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <!-- Dropdown for Conversion Type -->
+            <asp:Label ID="lblConversionType" runat="server" Text="Conversion Type:" />
+            <asp:DropDownList ID="ddlConversionType" runat="server">
+                <asp:ListItem Text="Celsius to Fahrenheit" Value="CtoF" />
+                <asp:ListItem Text="Fahrenheit to Celsius" Value="FtoC" />
+            </asp:DropDownList>
+            <br />
+            <br />
+            <!-- Button for Conversion -->
+            <asp:Button ID="btnConvert" runat="server" Text="Convert" OnClick="btnConvert_Click" />
+            <br /><br /> 
+            <!-- Label for Result -->
+            <asp:Label ID="lblResult" runat="server" CssClass="result-label"></asp:Label>
         </div>
     </form>
 </body>
--- Assighnment1/A2.aspx.cs
+++ Assighnment1/A2.aspx.cs
@@ -1,17 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
-namespace Assighment.Assighnment1
+namespace Tutorial.Assignment_1
 {
-    public partial class A2 : System.Web.UI.Page
+    public partial class Q2 : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void btnConvert_Click(object sender, EventArgs e)
         {
+            try
+            {
+                // Parse temperature input
+                double temperature = double.Parse(txtTemperature.Text);
 
+                // Determine conversion type
+                string conversionType = ddlConversionType.SelectedValue;
+                double convertedTemperature;
+
+                if (conversionType == "CtoF")
+                {
+                    // Celsius to Fahrenheit
+                    convertedTemperature = (temperature * 9 / 5) + 32;
+                    lblResult.Text = $"{temperature}°C is equal to {convertedTemperature:F2}°F";
+                }
+                else if (conversionType == "FtoC")
+                {
+                    // Fahrenheit to Celsius
+                    convertedTemperature = (temperature - 32) * 5 / 9;
+                    lblResult.Text = $"{temperature}°F is equal to {convertedTemperature:F2}°C";
+                }
+                else
+                {
+                    lblResult.Text = "Invalid conversion type selected.";
+                }
+            }
+            catch (FormatException)
+            {
+                lblResult.Text = "Please enter a valid numeric temperature.";
+            }
         }
     }
 }
